993b27d3d9d68699fd1dea0dc30a411551c16f65
wsky <wskyhx@gmail.com>
2012-08-30 15:09:17 +0800

    add team extensions; refact userhelper for aliuser

---
+++ src/Cooper.Web/AliExtensions/TeamController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CodeSharp.Core.Services;
+using Cooper.Model.Accounts;
+using Cooper.Model.Tasks;
+using Teams=Cooper.Model.Teams;
+using Cooper.Web.Controllers;
+using AliCooper.Model.Accounts;
+
+namespace Cooper.Web.AliExtensions
+{
+    /// <summary>扩展Team相关处理
+    /// </summary>
+    public class TeamController : Cooper.Web.Controllers.TeamController
+    {
+        private UserHelper _userHelper;
+        public TeamController(ILoggerFactory factory
+            , IAccountService accountService
+            , IAccountConnectionService accountConnectionService
+            , ITaskService taskService
+            , ITaskFolderService taskFolderService
+            , IFetchTaskHelper fetchTaskHelper
+            , Teams.ITeamService teamService
+            , Teams.ITaskService teamTaskService
+
+            , UserHelper userHelper)
+            : base(factory
+            , accountService
+            , accountConnectionService
+            , taskService
+            , taskFolderService
+            , fetchTaskHelper
+            , teamService
+            , teamTaskService)
+        {
+            this._userHelper = userHelper;
+        }
+
+        protected override string GetDefaultEmail(Account a)
+        {
+            var ark = this._accountConnectionService
+                .GetConnections(a)
+                .FirstOrDefault(o => o is ArkConnection) as ArkConnection;
+
+            if (ark == null)
+                return base.GetDefaultEmail(a);
+
+            var user = this._userHelper.GetUserByUserName(ark.Name);
+            return user != null ? user.Email : null;
+        }
+        protected override AccountConnection GetDefaultConnectionByEmail(string email)
+        {
+            var c = base.GetDefaultConnectionByEmail(email);
+            if (c != null)
+                return c;
+            var user = this._userHelper.GetUserByEmail(email);
+            return user != null
+                ? this._accountConnectionService.GetConnection<ArkConnection>(user.UserName)
+                : null;
+        }
+    }
+}
---
+++ src/Cooper.Web/AliExtensions/UserHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CodeSharp.Core.Services;
+using CodeSharp.Core;
+using System.Net;
+using System.Text;
+using System.IO;
+
+namespace Cooper.Web.AliExtensions
+{
+    [CodeSharp.Core.Component]
+    public class UserHelper
+    {
+        private static CodeSharp.ServiceFramework.DefaultJSONSerializer _jsonHelper = new CodeSharp.ServiceFramework.DefaultJSONSerializer();
+        private ILog _log;
+        private string _api_user;
+        private string _api_user_workid;
+        private string _api_user_email;
+        public UserHelper(ILoggerFactory factory
+            , string ali_api_user
+            , string ali_api_user_workid
+            , string ali_api_user_email)
+        {
+            this._log = factory.Create(typeof(UserHelper));
+            this._api_user = ali_api_user;
+            this._api_user_workid = ali_api_user_workid;
+            this._api_user_email = ali_api_user_email;
+        }
+        public Taobao.Facades.UserInfo GetUserByWorkId(string workId)
+        {
+            return this.WebGet<Taobao.Facades.UserInfo>(string.Format(this._api_user_workid, workId));
+        }
+        public Taobao.Facades.UserInfo GetUserByUserName(string username)
+        {
+            return this.WebGet<Taobao.Facades.UserInfo>(string.Format(this._api_user, username));
+        }
+        public Taobao.Facades.UserInfo GetUserByEmail(string email)
+        {
+            return this.WebGet<Taobao.Facades.UserInfo>(string.Format(this._api_user_email, email));
+        }
+        private T WebGet<T>(string url)
+        {
+            try
+            {
+                using (var wc = new WebClient() { Encoding = Encoding.UTF8 })
+                    return _jsonHelper.Deserialize<T>(wc.DownloadString(url));
+            }
+            catch (WebException e)
+            {
+                if (e.Response == null)
+                    this._log.Error(e);
+                using (var reader = new StreamReader(e.Response.GetResponseStream()))
+                    this._log.Error(reader.ReadToEnd(), e);
+            }
+            catch (Exception e)
+            {
+                this._log.Error(e);
+            }
+            return default(T);
+        }
+    }
+}
